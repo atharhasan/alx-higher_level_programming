b45867210f07541d305d419c9f1070839a8141cb
atharhasan <athar_h12@yahoo.com>
2023-11-28 09:33:13 +0200

    commit task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    return str.title()
+    msg = ''
+    for i in range(0, len(str)):
+        ch = str[i]
+        if 'a' <= ch <= 'z':
+            msg += '{}'.format(chr(ord(ch) - 32))
+        else:
+            msg += ch
+    print(msg)
+
