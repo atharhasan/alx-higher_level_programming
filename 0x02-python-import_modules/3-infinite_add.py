ae85b7b1345bfd11308ecbacb6f1f9cf0a16a1be
atharhasan <athar_h12@yahoo.com>
2023-11-30 20:23:58 +0200

    commit task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     argv = sys.argv
     arg = len(argv) - 1
     for i in range(1, len(argv)):
-        sum += int().argv[i]
-        print("{}".format(sum))
+        sum += int(argv[i])
+        print(f"{sum}")
