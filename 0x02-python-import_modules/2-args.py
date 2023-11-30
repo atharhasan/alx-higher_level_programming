20ed2bbdb5fd723d1e8a9bed290df8501fbe2e21
atharhasan <athar_h12@yahoo.com>
2023-11-30 19:53:16 +0200

    commit task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv
+    arg = len(argv) - 1
+    if arg == 0:
+        print("{} arguments.".format(arg))
+    elif arg == 1:
+        print("{} argument:".format(arg))
+    else:
+        print("{} arguments:".format(arg))
+    for i in range(0, arg):
+        print("{}: {}".format(i, arg[i]))
