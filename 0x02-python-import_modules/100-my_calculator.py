b877cb1d65b99e6d0fc61c1f7bb75ac342a967b6
atharhasan <athar_h12@yahoo.com>
2023-11-30 22:06:14 +0200

    commit task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1 as calc
+
+    arg = len(sys.argv)
+    if arg != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    ober = sys.argv[2]
+    b = int(sys.argv[3])
+    if ober == "+":
+        print("{} {} {} = {}".format(a, ober, b, a + b))
+    elif ober == "-":
+        print("{} {} {} = {}".format(a, ober, b, a - b))
+    elif ober == "*":
+        print("{} {} {} = {}".format(a, ober, b, a * b))
+    elif ober == "/":
+        print("{} {} {} = {}".format(a, ober, b, a / b))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
