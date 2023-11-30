1a3a129ed849c635fc60b226514f887187d1bba8
atharhasan <athar_h12@yahoo.com>
2023-11-30 20:18:04 +0200

    commit task

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    arg = len(argv) - 1
+    for i in range(1, len(argv)):
+        sum += argv[i]
+        print("{}".format(sum))
