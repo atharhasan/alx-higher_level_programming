4ede9101ffa11e4dbffc6ac883941a1b955c5701
atharhasan <athar_h12@yahoo.com>
2023-11-30 19:12:46 +0200

    commit task

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as ee
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, ee.add(a, b)))
+    print("{} + {} = {}".format(a, b, ee.sub(a, b)))
+    print("{} + {} = {}".format(a, b, ee.mul(a, b)))
+    print("{} + {} = {}".format(a, b, ee.div(a, b)))
