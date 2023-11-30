b9b9d29fa6b782a59c89a7bc3f7791ac99cc8411
atharhasan <athar_h12@yahoo.com>
2023-11-30 18:50:47 +0200

    commit task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, a + b))
-    return a + b
+from add_0 import add
+
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
