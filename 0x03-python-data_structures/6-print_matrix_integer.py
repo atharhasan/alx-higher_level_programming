6f81798829858a8986314e960678c08033882851
atharhasan <athar_h12@yahoo.com>
2023-12-02 22:35:47 +0200

    commit task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     n = len(matrix)
     for i in range(0, len(matrix)):
-        print("{}".format(matrix[i]).replace("[", "").replace("]", ""))
+        print("")
+        for j in range(0, len(matrix[i])):
+            print("{}".format(matrix[i][j]), end= ' ')
+    print("")
