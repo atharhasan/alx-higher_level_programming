6894da777c1002a3ff482437b3254726abd68e75
atharhasan <athar_h12@yahoo.com>
2023-12-02 22:02:46 +0200

    commit task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = my_string
-    for i in range(0, len(new_str)):
-        if new_str[i] == 'c' or new_str[i] == 'C':
-            new_str.remove(new_str[i])
+    for i in range(0, len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            new_str = my_string.remove(my_string[i])
     return new_str
