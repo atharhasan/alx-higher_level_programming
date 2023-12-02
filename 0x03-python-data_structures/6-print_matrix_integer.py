c2dcd9cf3cae124fae4e7718f7bca06f7ff8cd21
atharhasan <athar_h12@yahoo.com>
2023-12-02 22:36:46 +0200

    commit task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(0, len(matrix)):
         print("")
         for j in range(0, len(matrix[i])):
-            print("{}".format(matrix[i][j]), end= ' ')
+            print("{:d}".format(matrix[i][j]), end= ' ')
     print("")
