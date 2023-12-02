49803726229c5fc79704c95aff51e1465fcbeea3
atharhasan <athar_h12@yahoo.com>
2023-12-02 23:40:52 +0200

    commit task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    val = tuple_a + (0, 0)
+    val2 = tuple_b + (0, 0)
+    add1 = val[0] + val2[0]
+    add2 = val[1] + val2[1]
+    return (add1, add2)
