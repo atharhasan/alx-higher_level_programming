6597c3646df7e10427f4a3fbe22cf6838d372963
atharhasan <athar_h12@yahoo.com>
2023-12-02 21:55:13 +0200

    commit task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     for i in range(0, len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            my_string.remove(i)
+            my_string.remove(my_string[i])
         return my_string
