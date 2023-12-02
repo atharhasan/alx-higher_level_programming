a6900a4a6b9ca56f1ca93bec10f6b48d26e24043
atharhasan <athar_h12@yahoo.com>
2023-12-02 09:50:23 +0200

    commit task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    size = len(my_list) -1
+    size = len(my_list)
     for i in range(0, size):
-        print("{}".format(my_list[i]))
+        print(my_list[i])
