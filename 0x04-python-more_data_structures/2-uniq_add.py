9c8a98f3e64f8c36c7e98f5cba541f8c00c68467
atharhasan <athar_h12@yahoo.com>
2023-12-07 01:54:50 +0200

    commit task

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
-#!/bin/bash
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    n = set()
+    sum = 0
+    for i in my_list:
+        if i not in n:
+            n.add(i)
+            sum += i
+    return sum
