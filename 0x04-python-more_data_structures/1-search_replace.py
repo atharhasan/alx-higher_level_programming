16da76ffefa2d3d492503b90e29b4235fa4dfeb0
atharhasan <athar_h12@yahoo.com>
2023-12-07 01:07:46 +0200

    commit task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list
-    idx = new_list.find(search)
+    idx = new_list.index(search)
     if idx != -1 :
-        new_list.replace(idx, replace)
-    return new_list
+        new_list[idx] = replace
+        return new_list
