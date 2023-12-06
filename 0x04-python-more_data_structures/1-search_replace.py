05302668a68d3abacf4e906391521fcb50e18287
atharhasan <athar_h12@yahoo.com>
2023-12-07 00:45:45 +0200

    commit task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,6 @@
-#!/bin/bash
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list
+    idx = new_list.find(search)
+    new_list.replace(idx, replace)
+    return new_list
