822063d30a23a752f6ad58e279693830232cc7f2
atharhasan <athar_h12@yahoo.com>
2023-12-07 01:12:22 +0200

    commit task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     idx = new_list.index(search)
-    new_list[idx] = replace
+    if idx != -1:
+        new_list[idx] = replace
     return new_list
