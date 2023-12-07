2d55326981cbf0e50a110de60e1579a75b5f9b24
atharhasan <athar_h12@yahoo.com>
2023-12-07 04:07:27 +0200

    commit task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,8 @@
-#!/bin/bash
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        n = max(a_dictionary, key=a_dictionary.get)
+        return n
+    else:
+        return 'None'
+    
