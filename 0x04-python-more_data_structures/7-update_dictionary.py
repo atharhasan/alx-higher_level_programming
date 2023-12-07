b3a98527bfe738f29aacb7fb4b05c19ff006dd87
atharhasan <athar_h12@yahoo.com>
2023-12-07 03:23:35 +0200

    commit task

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,7 @@
-#!/bin/bash
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update({key: value})
+    return a_dictionary
