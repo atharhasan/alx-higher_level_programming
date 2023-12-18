e105ecb66813a728fa81b67a8ac5731152a141c6
atharhasan <athar_h12@yahoo.com>
2023-12-19 01:23:49 +0200

    commit task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except (ZeroDivisionError, TypeError):
-        return None
+        result = None
     finally:
         print("{}".format(result))
     return result
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,5 +10,6 @@ def list_division(my_list_1, my_list_2, list_length):
                 raise TypeError("wrong type")
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            
+            print("division by 0")
+        except 
             
