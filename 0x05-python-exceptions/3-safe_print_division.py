0782dde17569128c3db55dfc958db65213962409
atharhasan <athar_h12@yahoo.com>
2023-12-19 01:20:51 +0200

    commit task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,10 +3,9 @@ def safe_print_division(a, b):
     result = 0.0
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         return None
     finally:
         print("{}".format(result))
     return result
-
-        
+     
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        result = 0
+        try:
+            if type(my_list_1[i]) not in [int, float]:
+                raise TypeError("wrong type")
+            if type(my_list_2[i]) not in [int, float]:
+                raise TypeError("wrong type")
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            
+            
