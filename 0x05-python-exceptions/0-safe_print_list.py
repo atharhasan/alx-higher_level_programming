030b74a58d73ce3de7ba4181e078e17945368b5e
atharhasan <athar_h12@yahoo.com>
2023-12-19 00:23:21 +0200

    commit task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     y = 0
     try:
-        for i in range (x):
+        for i in range(x):
             print("{}".format(my_list[i]), end="")
             y += 1
         print()
         return y
-    except:
+    except IndexError:
         print()
         return y
