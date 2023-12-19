6357c0f07f84ff4e50cd608d44ed20bdb64fe445
atharhasan <athar_h12@yahoo.com>
2023-12-19 14:32:33 +0200

    commit task

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,7 +31,7 @@ class Square:
 
     def my_print(self):
         """that prints in stdout the square with the character #"""
-        for i in range (0, self.__size):
+        for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print()
         if self.__size == 0:
