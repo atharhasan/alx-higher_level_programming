14d772356d70ff2eb6b024950dc7db011fb33078
atharhasan <athar_h12@yahoo.com>
2024-01-09 03:00:48 +0200

    commit task

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''8-Rectangle.py'''
+
+
+class BaseGeometry:
+    """this class represents a base geometry"""
+
+    def area(self):
+        """method not implemented yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates a value as an integer
+        """
+        if (type(value) != int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherite from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
