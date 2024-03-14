0197051fce18e2f562563437551aaa0b73ea1792
atharhasan <athar_h12@yahoo.com>
2024-03-14 14:39:29 +0200

    commit task

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// print 3 lines by using an array of string and a loop
+
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+
+for (i = 0; i < args.length; i++) {
+  console.log(args[i]);
+}
