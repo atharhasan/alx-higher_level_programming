c3233745f2fa9998a2f8337962b10e1ec619c503
atharhasan <athar_h12@yahoo.com>
2024-03-14 14:12:37 +0200

    commit task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 // prints a message depending of the number of arguments
 
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
