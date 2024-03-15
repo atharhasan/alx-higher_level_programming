943cc5d8ecfa2a8b7a76b27fc6166b731ea40a53
atharhasan <athar_h12@yahoo.com>
2024-03-15 11:36:14 +0200

    commit task

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const Squarep = require('./5-square');
+
+// Write a class Square that defines a square and inherits from Rectangle
+
+class Square extends Squarep {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+
+module.exports = Square;
