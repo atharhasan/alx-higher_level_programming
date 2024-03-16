93602272a268acba10f15e52badee726af1f8070
atharhasan <athar_h12@yahoo.com>
2024-03-16 15:04:00 +0200

    commit task

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""Lists cities"""
+"""mysql state from dayae"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        charset="utf8",
+
+    dbnames = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
     )
-    cur = conn.cursor()
-    cur.execute(
+
+    query = dbnames.cursor()
+    query.execute(
         "SELECT cities.id, cities.name\
-        FROM cities INNER JOIN states ON cities.state_id = states.id\
-            WHERE states.name = %s",
+                  FROM cities INNER JOIN states ON cities.state_id=states.id\
+                  WHERE states.name = %s",
         [argv[4]],
     )
-    query_rows = cur.fetchall()
 
-    list = []
-    for i in query_rows:
-        list.append(i[1])
-        print(", ".join(list))
+    test = []
+    for i in query.fetchall():
+        test.append(i[1])
+    print(", ".join(test))
 
-    cur.close()
-    conn.close()
+    query.close()
+    dbnames.close()
